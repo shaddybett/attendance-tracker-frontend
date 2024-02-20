34af22a86f655ad865d57c98bad22af345d610ab
tray <onesimus.githua@student.moringaschool.com>
2024-02-20 10:12:23 +0300

    created a student profile form

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Form from './Form'
+import ProfileForm from './Profile'
 
 
 function App() {
   return (
     <div>
-      <h1>Application</h1>
       <Form />
+      <ProfileForm />
     </div>
     
   )
